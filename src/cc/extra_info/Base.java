4fe4b653d3787cde3e6805301bb1d15eab89ca58
shuailong <xushuailong@111.com.cn>
2018-05-24 18:35:55 +0800

    update

---
+++ src/cc/extra_info/Base.java
@@ -0,0 +1,39 @@
+package cc.extra_info;
+
+import static java.lang.System.out;
+
+public class Base {
+    public static void main(String[] args) {
+        {
+            // 匿名内部类
+            new Object() {
+                void test() {
+                    out.println("lalalal");
+                }
+            }.test();
+
+            Object test = new Object() {
+                void test() {
+                    out.println("hahahah");
+                }
+            };
+//        test.test();
+        }
+
+        {
+            // 异常的捕获
+            // 这种情况，如果出现异常，并不处理，但是资源一定关闭，
+            // 所以try  finally集合只为关闭资源
+            int zero = 0;
+            try {
+                int a = 5 / zero;
+                if (zero != 0) {
+                    throw new RuntimeException("the zero is not equals 0!");
+                }
+            } finally {
+                out.println("the finally run!");
+            }
+            out.println("the base end!");
+        }
+    }
+}
