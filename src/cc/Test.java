105ad16e21c34604c9e8f2cfeb3df133010095f8
shuailong <xushuailong@111.com.cn>
2018-05-28 19:08:04 +0800

    add test

---
+++ src/cc/Test.java
@@ -0,0 +1,28 @@
+package cc;
+
+class Test {
+    public static void main(String[] args) {
+        String s1 = "java";
+        String s2 = "hello";
+        method_1(s1, s2);
+        System.out.println(s1 + "...." + s2); //java....hello
+
+        StringBuilder s11 = new StringBuilder("java");
+        StringBuilder s22 = new StringBuilder("hello");
+        method_2(s11, s22);
+        System.out.println(s11 + "-----" + s22); //javahello-----hello
+
+
+    }
+
+    public static void method_1(String s1, String s2) {
+        s1.replace('a', 'k');
+        s1 = s2;
+    }
+
+    public static void method_2(StringBuilder s1, StringBuilder s2) {
+        s1.append(s2);
+        s1 = s2;
+    }
+}
+
