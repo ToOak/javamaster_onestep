30218a5ce7902438e508fc78eec00d48ee11977d
afei <xuafeiJelly@163.com>
2018-06-06 22:48:55 +0800

    md5

---
+++ src/cc/extra_info/MD5AndSHA.java
@@ -0,0 +1,15 @@
+package cc.extra_info;
+
+import static java.lang.System.out;
+
+public class MD5AndSHA {
+	public static void main(String[] args) {
+		out.println(Integer.toHexString("Hello World!".hashCode()));
+		out.println(Integer.toHexString(new String("Hello World!").hashCode()));
+		out.println(Integer.toHexString(new StringBuffer("Hello World!").hashCode()));
+		out.println(Integer.toHexString(new StringBuffer("Hello World!").toString().hashCode()));
+		out.println(Integer.toHexString(new StringBuilder("Hello World!").hashCode()));
+		out.println(Integer.toHexString(new StringBuilder("Hello World!").toString().hashCode()));
+	}
+
+}
