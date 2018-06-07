16b2dcaffb2df2c21eae20b3ac6206cfb89313c9
afei <xuafeiJelly@163.com>
2018-06-07 20:56:30 +0800

    update

--- src/cc/extra_info/MD5AndSHA.java
+++ src/cc/extra_info/MD5AndSHA.java
@@ -2,6 +2,7 @@ package cc.extra_info;
 
 import static java.lang.System.out;
 
+
 public class MD5AndSHA {
 	public static void main(String[] args) {
 		out.println(Integer.toHexString("Hello World!".hashCode()));
@@ -10,6 +11,7 @@ public class MD5AndSHA {
 		out.println(Integer.toHexString(new StringBuffer("Hello World!").toString().hashCode()));
 		out.println(Integer.toHexString(new StringBuilder("Hello World!").hashCode()));
 		out.println(Integer.toHexString(new StringBuilder("Hello World!").toString().hashCode()));
+
 	}
 
 }
